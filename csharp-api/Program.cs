a9ed4f82956ad127180fa064016e2a7ea9b330e4
Pete Voulgaris <petevoulgaris@gmail.com>
2026-07-30 21:50:07 -0400

    got this sample working

--- csharp-api/Program.cs
+++ csharp-api/Program.cs
@@ -1,10 +1,12 @@
-using Microsoft.Data.SqlClient;
 using Dapper;
+using Microsoft.Data.SqlClient;
+using PoolMonitoringApi;
 
 const string serviceName = "pool-monitoring-api";
 var builder = WebApplication.CreateBuilder(args);
 
-var connString = "Server=sqlserver2025;Database=master;User Id=sa;Password=YourSecurePassword123!;Max Pool Size=150;TrustServerCertificate=True;";
+var connString =
+    "Server=sqlserver2025;Database=master;User Id=sa;Password=YourSecurePassword123!;Max Pool Size=150;TrustServerCertificate=True;";
 
 builder.AddAppTelemetryV2(serviceName);
 
@@ -12,21 +14,29 @@ var app = builder.Build();
 app.UseOpenTelemetryPrometheusScrapingEndpoint();
 
 // Endpoint Route 1
-app.MapGet("/v1/data-endpoint", async () =>
-{
-    using var connection = new SqlConnection(connString);
-    var result = await connection.QueryAsync<int>("WAITFOR DELAY '00:00:00.100'; SELECT 1;");
-    return Results.Ok(new { status = "Success", data = result });
-});
+app.MapGet(
+    "/v1/data-endpoint",
+    async () =>
+    {
+        using var connection = new SqlConnection(connString);
+        var result = await connection.QueryAsync<int>("WAITFOR DELAY '00:00:00.100'; SELECT 1;");
+        return Results.Ok(new { status = "Success", data = result });
+    }
+);
 
 // Endpoint Route 2
-app.MapGet("/v1/admin-report", async () =>
-{
-    using (var connection = new SqlConnection(connString))
+app.MapGet(
+    "/v1/admin-report",
+    async () =>
     {
-        var result = await connection.QueryAsync<int>("WAITFOR DELAY '00:00:00.050'; SELECT 2;");
-        return Results.Ok(new { status = "Admin Success", data = result });
+        using (var connection = new SqlConnection(connString))
+        {
+            var result = await connection.QueryAsync<int>(
+                "WAITFOR DELAY '00:00:00.050'; SELECT 2;"
+            );
+            return Results.Ok(new { status = "Admin Success", data = result });
+        }
     }
-});
+);
 
 app.Run();
